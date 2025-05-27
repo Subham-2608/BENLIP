2347f3611877ed808e396cd22fa296b0d7eef574
Subham-2608 <subhamghosh316@gmail.com>
2025-05-27 19:21:36 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,13 +4,13 @@ st.set_page_config( page_title="BENLiP", initial_sidebar_state="expanded", layou
 col1, col2, col3 = st.columns([1.5, 20, 2])
 
 with col1:
-    st.image("static/images/icarlogo.png", width=230)
+    st.image("static/images/icarlogo.png", width=150)
 
 with col2:
     st.markdown("<h1 style='text-align:center;'> BENLiP: Bagging based Ensemble model to identify Nucleosome and Linker Positioning</h1>", unsafe_allow_html=True)
 
 with col3:
-    st.image("static/images/iasri-logo.png", width=230)
+    st.image("static/images/iasri-logo.png", width=150)
 
 st.markdown("---")
 st.text("")
