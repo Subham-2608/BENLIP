173c2b1ba5afedb026df0182f629bce109b11f17
Subham-2608 <subhamghosh316@gmail.com>
2026-04-17 19:34:44 +0530

    Update 5_acknowledge.py

--- pages/5_acknowledge.py
+++ pages/5_acknowledge.py
@@ -2,15 +2,36 @@ import streamlit as st
 
 st.set_page_config(page_title="Acknowledgement", layout="wide")
 
-# Title
-st.markdown("<h1 style='text-align:center;'>Acknowledgement</h1>", unsafe_allow_html=True)
+# Top Header with Logos and Title
+col1, col2, col3 = st.columns([1.5, 20, 2])
+
+with col1:
+    st.image("static/images/icarlogo.png", width=120)
+
+with col2:
+    st.markdown(
+        "<h2 style='text-align:center;'>BENLiP: Bagging based Ensemble Model to Identify Nucleosome and Linker Positioning</h2>",
+        unsafe_allow_html=True
+    )
+
+with col3:
+    st.image("static/images/iasri-logo.png", width=120)
+
 st.markdown("---")
 
-# Acknowledgement text
+# Acknowledgement Title
+st.markdown("<h1 style='text-align:center;'>Acknowledgement</h1>", unsafe_allow_html=True)
+
+st.markdown("---")
+
+# Acknowledgement Content
 st.markdown("""
-The authors gratefully acknowledge the financial support provided by the **Department of Biotechnology (DBT), Government of India, New Delhi**,  
-under the project **No. BT/PR40191/BTIS/137/82/2023**.
-""")
+<div style='text-align:center; font-size:18px;'>
+
+The authors gratefully acknowledge the financial support provided by the <b>Department of Biotechnology (DBT), Government of India, New Delhi</b>, under the project <b>No. BT/PR40191/BTIS/137/82/2023</b>.
+
+</div>
+""", unsafe_allow_html=True)
 
 st.markdown("---")
 
